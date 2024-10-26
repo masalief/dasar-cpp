d2585b63614d6df2ed9ac7d2623876e777439494
masalief <aliefep154@gmail.com>
2024-10-26 13:17:19 +0700

    new file:   .vscode/settings.json 	new file:   modul-10-for.cpp 	new file:   modul-10-for.exe 	new file:   modul-10-while.cpp 	new file:   modul-10-while.exe

---
+++ modul-10-for.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Bilangan Ganjil sampai 35: ";
+    for (int i = 1; i <= 35; i++) {
+        if (i % 2 != 0) {
+            cout << i << " ";
+        }
+    }
+
+    cout << "\nBilangan Genap sampai 35: ";
+    for (int i = 1; i <= 35; i++) {
+        if (i % 2 == 0) {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
---
+++ modul-10-while.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+
+    cout << "Bilangan Ganjil sampai 35: ";
+    while (i <= 35) {
+        if (i % 2 != 0) {
+            cout << i << " ";
+        }
+        i++;
+    }
+
+    i = 1; // Reset nilai i untuk bilangan genap
+    cout << "\nBilangan Genap sampai 35: ";
+    while (i <= 35) {
+        if (i % 2 == 0) {
+            cout << i << " ";
+        }
+        i++;
+    }
+
+    return 0;
+}
